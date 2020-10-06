bf5fcaad983e72ab22d74d5d0feec75a9e7a568e
user <joshmanuel1996@gmail.com>
2020-10-06 15:47:54 -0400

    completed projects page and modified bottom nav

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,7 @@ router.get('/home', function(req, res, next) {
 
 /* GET about page. */
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About Us' });
+  res.render('about', { title: 'About' });
 });
 
 /* GET products page. */
