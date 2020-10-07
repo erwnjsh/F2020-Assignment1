d53b4fc8eefed98911b325cea1848683e2ef9c68
user <joshmanuel1996@gmail.com>
2020-10-07 19:22:49 -0400

    scripts.app.js to allow capture of data from contact form

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,5 +1,5 @@
 // BEGIN DOCUMENT HEADER
-// FILE NAME    : app.js
+// FILE NAME    :   app.js
 // AUTHOR NAME  :   Erwin Joshua Manuel
 // STUDENT ID   :   301107750
 // DATE CREATED :   10/5/2020
@@ -12,10 +12,45 @@
     function Start()
     {
         console.log("App Started...");
+
+        if(document.title === "Contact")
+        {
+            let sendButton = document.getElementById("sendButton");
+            let cancelButton = document.getElementById("cancelButton");
+            let form = document.forms[0];
+
+            // SENDBUTTON CLICK
+            sendButton.addEventListener("click",(event) => {
+                event.preventDefault();
+
+                // CAPTURING VALUES FROM FORM
+                let FName = document.getElementById("FName").value;
+                let LName = document.getElementById("LName").value;
+                let Email = document.getElementById("Email").value;
+                let TelNum = document.getElementById("TelNum").value;
+                let Message = document.getElementById("Message").value;
+
+                // OUTPUTTING VALUES ON CONSOLE
+                console.info(`First Name     : ${FName}
+                Last Name      : ${LName}
+                Email          : ${Email}
+                Phone Number   : ${TelNum}
+                Message        : ${Message}`);
+
+                form.reset();
+            });
+
+            // CANCEL BUTTON CLICK
+            cancelButton.addEventListener("click",(event) => {
+                event.preventDefault();
+                if(confirm("Are you sure?"))
+                {
+                    location.href = "/";
+                }
+            });
+        }
     }
 
     window.addEventListener("load", Start);
-
-    // FUNCTION TO CAPTURE USER INPUT FROM CONTACTS
 
 })();
