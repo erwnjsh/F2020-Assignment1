9fe74755219133e622dd7fa89012631381f484fb
user <joshmanuel1996@gmail.com>
2020-10-06 21:25:20 -0400

    added comments for internal documentation

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,3 +1,11 @@
+// BEGIN DOCUMENT HEADER
+// FILE NAME    : app.js
+// AUTHOR NAME  :   Erwin Joshua Manuel
+// STUDENT ID   :   301107750
+// DATE CREATED :   10/5/2020
+// END DOCUMENT HEADER
+
+
 // IIFE - IMMEDIATELY INVOKED FUNCTION EXPRESSION
 (function(){
 
@@ -7,5 +15,7 @@
     }
 
     window.addEventListener("load", Start);
+
+    // FUNCTION TO CAPTURE USER INPUT FROM CONTACTS
 
 })();
