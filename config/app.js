d1ccbf4a5cb8dc133d98ed4152c7494e3cd985d5
user <joshmanuel1996@gmail.com>
2020-10-18 11:00:18 -0400

    created contacts model, linked model to route, linked route to app

--- config/app.js
+++ config/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('../routes/index');
 var usersRouter = require('../routes/users');
+let booksRouter = require('../routes/contact');
 
 var app = express();
 
@@ -35,6 +36,7 @@ app.use(express.static(path.join(__dirname, '../node_modules')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/contact-list', booksRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/contact.js
@@ -0,0 +1,22 @@
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+
+//CONNECT TO CONTACT MODEL
+let Contact = require('../models/contact');
+
+/* GET ROUTE FOR THE CONTACTS LIST PAGE - READ OPERATION */
+router.get('/', (req, res, next) => {
+    Contact.find((err, ContactList) => {
+        if(err)
+        {
+            return console.error(err);
+        }
+        else
+        {
+            console.log(ContactList);
+        }
+    });
+});
+
+module.exports = router;
